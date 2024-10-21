c2edba43dc2a8fad8cd6f4712a3758129a6673fd
Joy <joyantamondal156@gmail.com>
2024-10-21 13:13:06 +0530

    Initial Commit

---
+++ LearningSourceControl/LearningSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSourceControlApp.swift
+//  LearningSourceControl
+//
+//  Created by Joyanta Mondal on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
